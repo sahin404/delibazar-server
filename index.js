38561f62422f0317eaccbcead5e58489dd999a73
Md Sahin Alam <sahinraj20@gmail.com>
2025-04-16 18:33:19 +0600

    make admin api

--- index.js
+++ index.js
@@ -171,6 +171,18 @@ async function run() {
       res.send(result);
     })
 
+    app.patch('/userMakeAdmin/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set:{
+          role:'admin'
+        }
+      }
+      const result = await users.updateOne(query, updateDoc);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
