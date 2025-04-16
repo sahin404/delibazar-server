079928fc1f936ad1f8d5dbcdfc0c816e3dea5013
Md Sahin Alam <sahinraj20@gmail.com>
2025-04-16 18:46:06 +0600

    remove admin functon complete

--- index.js
+++ index.js
@@ -183,6 +183,19 @@ async function run() {
       res.send(result);
     })
 
+
+    app.patch('/userRemoveAdmin/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set:{
+          role:''
+        }
+      }
+      const result = await users.updateOne(query, updateDoc);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
