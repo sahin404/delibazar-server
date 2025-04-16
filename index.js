079c4642932d0921203c0dc247a6459a79dafc1c
Md Sahin Alam <sahinraj20@gmail.com>
2025-04-16 16:12:03 +0600

    data api create for update

--- index.js
+++ index.js
@@ -145,6 +145,13 @@ async function run() {
       const result = await products.deleteOne(query);
       res.send(result);
     })
+
+    app.get('/update/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = products.findOne(query);
+      res.send(result);
+    })
     
 
     // Send a ping to confirm a successful connection
