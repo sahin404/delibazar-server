0671ba35fb61241d6f156234a6458657c677607b
Md Sahin Alam <sahinraj20@gmail.com>
2025-04-16 15:54:24 +0600

    product delete api create

--- index.js
+++ index.js
@@ -138,6 +138,13 @@ async function run() {
       const result = products.insertOne(newProduct);
       res.send(result);
     })
+
+    app.delete('/productDelete/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await products.deleteOne(query);
+      res.send(result);
+    })
     
 
     // Send a ping to confirm a successful connection
